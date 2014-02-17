30fa234cb33d451c4926ee9ab332dea6496dd19e
Rolf Bislin <romibi2@gmail.com>
2014-02-17 18:18:44 +0100

    meine uebung 1 aufgabe 1

---
+++ src/romibi/uebung01/aufgabe01/Main.java
@@ -0,0 +1,16 @@
+package romibi.uebung01.aufgabe01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new ThreadTicker().start();
+		new Thread(new RunnableTicker()).start();
+		new Thread() { public void run() {
+			try {
+				ConsoleTicker.periodTicker('c', 20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+}
